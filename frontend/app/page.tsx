d8dd9a9612bae76242b99de256eca20a967d3245
Mohammad Kermani <mkermani144@gmail.com>
2024-03-11 14:08:04 +0330

    feat: initialize frontend project

---
+++ frontend/app/page.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen gap-16">
+      <div className="border-2 border-solid border-blue-300 w-[48rem] h-64">
+        {/* Validations come here */}
+      </div>
+      <div className="border-2 border-solid border-black-300 w-96 h-96">
+        {/* QR code comes here */}
+      </div>
+    </main>
+  );
+};
+
+export default Home;
