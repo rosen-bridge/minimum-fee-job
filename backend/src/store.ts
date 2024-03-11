9fead6cb8abdfe9f8a9b730a044487e7864b2c31
Mohammad Kermani <mkermani144@gmail.com>
2024-03-11 12:25:19 +0330

    fix: fix issue in returned promise of store apis

--- backend/src/store.ts
+++ backend/src/store.ts
@@ -10,21 +10,21 @@ await client.connect();
  * save price data into store
  */
 const savePrices = async (prices: Map<string, number>) => {
-  return void client.hSet('prices', prices);
+  return void (await client.hSet('prices', prices));
 };
 
 /**
  * save tx chunks data into store
  */
 const saveTx = async (chunks: string[]) => {
-  return void client.hSet('tx.chunks', chunks);
+  return void (await client.hSet('tx.chunks', chunks));
 };
 
 /**
  * flush store, removing price and tx chunks data
  */
 const flushStore = async () => {
-  return void client.flushDb();
+  return void (await client.flushDb());
 };
 
 export { flushStore, savePrices, saveTx };
