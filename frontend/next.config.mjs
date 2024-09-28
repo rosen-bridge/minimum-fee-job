c35e561206e6920ba4372fed45e9bf9b9e888a6b
Mohammad Kermani <mkermani144@gmail.com>
2024-09-28 10:12:33 +0000

    chore: add wasm configurations to next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["ergo-lib-wasm-nodejs"],
   },
+  webpack: function (config, options) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      syncWebAssembly: true,
+      layers: true,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
