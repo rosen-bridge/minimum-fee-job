e1c7938c498e5f907f631a295bbd0c27699002a4
Mohammad Kermani <mkermani144@gmail.com>
2024-09-30 13:06:23 +0000

    chore: improve fallback style

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,17 +2,42 @@ import { Suspense } from "react";
 
 import Qr from "./_components/qrcode/Qr";
 import Validations from "./Validations";
+import { Box, CircularProgress } from "@mui/material";
 
 const Home = () => {
   return (
     <main className="flex flex-row items-center justify-center h-screen gap-16">
-      <div className="border-2 border-solid border-slate-300 shadow-xl rounded-2xl w-[48rem] max-h-[80vh]">
-        <Suspense fallback="Loading prices...">
+      <div className="border-2 border-solid border-slate-300 shadow-xl rounded-2xl w-[48rem] min-h-[380px] max-h-[80vh]">
+        <Suspense
+          fallback={
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              minHeight={380}
+            >
+              Loading validations &nbsp;
+              <CircularProgress size={12} />
+            </Box>
+          }
+        >
           <Validations />
         </Suspense>
       </div>
-      <div className="border-2 border-solid border-slate-300 shadow-xl rounded-2xl w-[380px]">
-        <Suspense fallback="Loading tx...">
+      <div className="border-2 border-solid border-slate-300 shadow-xl rounded-2xl w-[380px] min-h-[380px]">
+        <Suspense
+          fallback={
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              minHeight={380}
+            >
+              Loading tx &nbsp;
+              <CircularProgress size={12} />
+            </Box>
+          }
+        >
           <Qr />
         </Suspense>
       </div>
