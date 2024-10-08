e64ded2d15ae522553bb99d2d16f64703a4e9679
Mohammad Kermani <mkermani144@gmail.com>
2024-10-08 10:24:45 +0000

    fix: handle the case of KV env vars not being set in backend

--- backend/src/store.ts
+++ backend/src/store.ts
@@ -4,37 +4,40 @@ import { kvRestApiUrl, kvRestApiToken } from './configs';
 
 import { SupportedTokenConfig } from './types';
 
-const kv = createClient({
-  url: kvRestApiUrl,
-  token: kvRestApiToken,
-});
+const kv =
+  kvRestApiUrl && kvRestApiToken
+    ? createClient({
+        url: kvRestApiUrl,
+        token: kvRestApiToken,
+      })
+    : null;
 
 /**
  * save tokens config into store
  */
 const saveTokensConfig = async (config: SupportedTokenConfig[]) => {
-  return void (await kv.set('tokens-config', JSON.stringify(config)));
+  return void (await kv?.set('tokens-config', JSON.stringify(config)));
 };
 
 /**
  * save price data into store
  */
 const savePrices = async (prices: Map<string, number>) => {
-  return void (await kv.hset('prices', Object.fromEntries(prices)));
+  return void (await kv?.hset('prices', Object.fromEntries(prices)));
 };
 
 /**
  * save tx chunks data into store
  */
 const saveTx = async (tx: string) => {
-  return void (await kv.set('tx', tx));
+  return void (await kv?.set('tx', tx));
 };
 
 /**
  * flush store, removing price and tx chunks data
  */
 const flushStore = async () => {
-  return void (await kv.flushdb());
+  return void (await kv?.flushdb());
 };
 
 export { flushStore, saveTokensConfig, savePrices, saveTx };
