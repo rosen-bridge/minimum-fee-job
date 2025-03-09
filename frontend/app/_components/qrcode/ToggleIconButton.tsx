aedbcbf4c69e1eec583267d93863818589076fc6
Masood Abdolian <m.abdolian@gmail.com>
2025-03-09 17:35:09 +0330

    feat: Enhance the QR code display component to streamline the scanning process

---
+++ frontend/app/_components/qrcode/ToggleIconButton.tsx
@@ -0,0 +1,26 @@
+import { IconButton, IconButtonProps, styled } from '@mui/material';
+ 
+interface ToggleIconButtonPropsType extends IconButtonProps {
+  selected?: boolean;
+}
+
+const StyledIconButton = styled(IconButton)(({ theme }) => ({
+  '&.selected': {
+    backgroundColor: `${theme.palette.primary.light}80`,
+  },
+}));
+
+export const ToggleIconButton = ({
+  selected,
+  color = 'primary',
+  className,
+  ...restProps
+}: ToggleIconButtonPropsType) => {
+  return (
+    <StyledIconButton
+      {...restProps}
+      color={selected ? color : 'default'}
+      className={(selected ? 'selected ' : '') + className}
+    />
+  );
+};
