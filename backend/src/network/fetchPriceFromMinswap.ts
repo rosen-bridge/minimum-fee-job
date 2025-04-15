f77098b92d0bb7093f415941d625d06b8ba54f55
HFazelinia <hadifazelinia78@gmail.com>
2025-04-15 08:40:33 +0100

    add minswap

---
+++ backend/src/network/fetchPriceFromMinswap.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { urls } from '../configs';
+
+const axiosMinswap = axios.create({
+  baseURL: urls.minswap,
+  timeout: 8000,
+});
+
+export const fetchPriceFromMinswapInADA = async (
+  tokenId: string,
+  lpPolicyId: string,
+  lpAssetName: string
+): Promise<number> => {
+  const [policyId, assetName] = tokenId.split('.');
+  const response = await axiosMinswap.post(
+    '',
+    {
+      query:
+        'query PriceChart($input: PriceChartInput!) {\n  priceChart(input: $input) {\n    time\n    value\n  }\n}',
+      variables: {
+        input: {
+          assetIn: {
+            currencySymbol: '',
+            tokenName: '',
+          },
+          assetOut: {
+            currencySymbol: policyId,
+            tokenName: assetName,
+          },
+          lpAsset: {
+            currencySymbol: lpPolicyId,
+            tokenName: lpAssetName,
+          },
+          period: 'ONE_YEAR',
+        },
+      },
+      operationName: 'PriceChart',
+    },
+    { params: { PriceChart: '' } }
+  );
+  return Number(response.data.data.priceChart[0].value);
+};
